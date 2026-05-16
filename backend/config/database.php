5943c131f026d1c9633d3cdd710e6a137072a6f8
Your Name <you@example.com>
2026-05-16 14:37:29 +0300

    new fixes

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,57 +1,27 @@
 <?php
 
-use Illuminate\Support\Str;
-
 return [
-    'default' => env('DB_CONNECTION', 'sqlite'),
+
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
 
-    'pgsql' => [
-        'driver' => 'pgsql',
-
-        'host' => env('DB_HOST'),
-        'port' => (int) env('DB_PORT', 5432),
-        'database' => env('DB_DATABASE'),
-        'username' => env('DB_USERNAME'),
-        'password' => env('DB_PASSWORD'),
-
-        'charset' => 'utf8',
-        'prefix' => '',
-        'prefix_indexes' => true,
-        'search_path' => 'public',
-        'sslmode' => 'require',
-
-        'options' => extension_loaded('pdo_pgsql') ? array_filter([]) : [],
-    ],
-    'sqlite' => [
-        'driver' => 'sqlite',
-        'url' => null,
-        'database' => env('DB_DATABASE', database_path('database.sqlite')),
-        'prefix' => '',
-        'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-        ],
-
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => null,
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST'),
+            'port' => env('DB_PORT'),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'search_path' => 'public',
+            'sslmode' => 'prefer',
         ],
+
     ],
 
     'migrations' => 'migrations',
+
 ];
