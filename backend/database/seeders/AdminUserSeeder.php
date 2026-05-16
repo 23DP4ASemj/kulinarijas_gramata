2e0952afa082a7c683ba2d2759cccbc5468ad3fe
Your Name <you@example.com>
2026-05-16 15:59:24 +0300

    make administator

--- backend/database/seeders/AdminUserSeeder.php
+++ backend/database/seeders/AdminUserSeeder.php
@@ -9,24 +9,13 @@ use Illuminate\Support\Facades\Hash;
 class AdminUserSeeder extends Seeder
 {
     public function run(): void
-{
+    {
         User::updateOrCreate(
             ['email' => 'test@test.test'],
             [
                 'name' => 'Admin',
                 'password' => Hash::make('test111'),
                 'role' => 'admin',
-            ]
-        );
-
-
-        User::updateOrCreate(
-            ['email' => $adminEmail],
-            [
-                'name' => $adminName,
-                'password' => Hash::make($adminPassword),
-                'role' => User::ROLE_ADMIN,
-                'email_verified_at' => now(),
             ]
         );
     }
